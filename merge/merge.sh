6b83df2421010450e15f522efa87436681c77f71
tzx <district10@users.noreply.github.com>
2015-09-24 22:01:23 +0800

    merge inplace, rm after merging

--- merge/merge.sh
+++ merge/merge.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-OUT="../merged.txt"
+OUT="merged.tmp"
 echo "" > $OUT
 
 for i in *.txt;
 do
     echo merging $i
     cat $i >> $OUT
+    rm $i
 done
 
-echo merged to $OUT
+mv $OUT ${OUT%.*}.txt
+echo merged to ${OUT%.*}.txt
